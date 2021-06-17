11f540beea2cb2194db00e6abf9598402314675e
harshasavanth <savanthharsha@github.com>
2021-06-17 20:14:46 +0530

    commit

---
+++ src/test/java/com/harsha/beerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,33 @@
+package com.harsha.beerservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeerById() {
+    }
+}
