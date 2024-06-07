3627d8e7a82aa8955630ec991033f00ca380be7f
Harsha N Savanth <Harsha.NSavanth@cognizant.com>
2024-06-07 20:20:10 +0530

    added Exception Handling. closes #8

--- src/main/java/com/microservice/beerservice/web/controller/BeerController.java
+++ src/main/java/com/microservice/beerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.microservice.beerservice.web.controller;
 import com.microservice.beerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,12 +20,12 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
