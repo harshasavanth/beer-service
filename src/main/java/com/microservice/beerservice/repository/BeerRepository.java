b3549ebea52f46c1d96720e0375a5ae4e1b6f4c6
Harsha N Savanth <Harsha.NSavanth@cognizant.com>
2024-06-07 13:01:32 +0530

    adding bootstrap. closes #6

--- src/main/java/com/microservice/beerservice/repository/BeerRepository.java
+++ src/main/java/com/microservice/beerservice/repository/BeerRepository.java
@@ -1,9 +1,10 @@
 package com.microservice.beerservice.repository;
 
 import com.microservice.beerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 }
