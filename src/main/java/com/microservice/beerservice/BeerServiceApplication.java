338badf7079c1c1de1075e373e4182e49d6c324d
Harsha N Savanth <Harsha.NSavanth@cognizant.com>
2024-06-07 12:03:00 +0530

    adding data model. closes #1

--- src/main/java/com/microservice/beerservice/BeerServiceApplication.java
+++ src/main/java/com/microservice/beerservice/BeerServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservice.beer_service;
+package com.microservice.beerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/microservice/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.microservice.beerservice.web.model;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEATE, IPA, PALE_ALE, SAISON
+}
