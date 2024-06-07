d753b6dac988e938a27876c27481620033daa7ef
Harsha N Savanth <Harsha.NSavanth@cognizant.com>
2024-06-07 11:46:54 +0530

    initial commit

---
+++ src/main/java/com/microservice/beer_service/BeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.beer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceApplication.class, args);
+	}
+
+}
