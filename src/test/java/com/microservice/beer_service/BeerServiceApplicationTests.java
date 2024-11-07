d719e582374172fdbf4169aef470c91cc8f8ca45
Harsha Savanth <harshansavanth123@gmail.com>
2024-11-07 18:53:55 +0530

    initial commit

---
+++ src/test/java/com/microservice/beer_service/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.beer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
